860179de1bac819505fc45ed6e60332978514c84
Kopo Ntheledi <kopontheledi@gmail.com>
2023-10-02 09:48:39 +0200

    added new markdown, new css, and an image

--- my-app/components/posts/post-detail/post-content.js
+++ my-app/components/posts/post-detail/post-content.js
@@ -1,6 +1,6 @@
 import classes from './post-detail.module.css';
 import ReactMarkdown from 'react-markdown';
-
+import Image from 'next/image';
 import PostHeader from './post-header';
 
 
@@ -11,10 +11,42 @@ export default function PostContent(props) {
 const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
 
+const customRenderers = {
+    // image(image) {
+    //     return (
+    //     <Image src={`/images/posts/${post.slug}/${image.src}`} alt={image.alt} width={600} height={300} />
+    //     );
+    // },
+
+paragraph(paragraph) {
+
+    const {node} = paragraph;
+    if (node.children[0].type === 'image') {
+       const image = node.children[0]; 
+
+       return (
+       <div className={classes.image} >
+        <Image 
+        src={`/images/posts/${post.slug}/${image.url}`} 
+        alt={image.alt} 
+        width={600} 
+        height={300} 
+        />
+       </div>
+       );
+    }
+    return <p>{paragraph.children}</p>;
+},
+
+code(code) {
+    return
+}
+};
+
     return(
         <article className={classes.content} >
             <PostHeader title={post.title} image={imagePath} />
-            <ReactMarkdown>{post.content}</ReactMarkdown>
+            <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
         </article>
     )
 }
